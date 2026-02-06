22cb5f4f5168ab871a129fe34a4e8cb0d587c189
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-02-06 11:08:01 +0600

    generate text state of a sentence

--- phonebook-prefix.js
+++ phonebook-prefix.js
@@ -21,3 +21,14 @@ function searchByPrefix(phonebook, prefix) {
 
 let searchResult = searchByPrefix({ rahim: "0181", karim: "0172", rafi: "0193" }, "ra");
 console.log(searchResult);
+
+// Test case 2
+// Input:
+// phonebook = { mina: "013", mim: "014", rina: "015" };
+// prefix = "mi";
+
+// Output:
+// ["mina", "mim"]
+
+searchResult = searchByPrefix({ mina: "013", mim: "014", rina: "015" }, "mi");
+console.log(searchResult);
---
+++ text-state-generator.js
@@ -0,0 +1,18 @@
+// Problem 7: Text Stats Generator
+// Function name: textStats(text)
+// Statement:
+//  Return an object containing:
+// Characters count excluding spaces
+
+// Words count
+
+// Vowels count
+
+// Consonants count
+
+// Test case 1
+// Input:
+//  "JavaScript is fun to learn"
+// Output:
+// { characters: 22, words: 5, vowels: 8, consonants: 14 }
+
